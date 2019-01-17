c18cc98a1f1d5ae37dd4ba570b72581e984ee6e6
Lorna Ryan <lornaniriain@gmail.com>
2019-01-16 19:30:26 -0500

    return bars object from DB

--- src/bars.js
+++ src/bars.js
@@ -1,10 +1,10 @@
 var _ = require('lodash');
 
 function convertDBBarObjectsToJSON(bars) {
-    return JSON.stringify( _.reduce(bars, (accumulator, bar, index) => {
+    return JSON.stringify({bars: _.reduce(bars, (accumulator, bar, index) => {
         accumulator.push(_.pick(bar, ['bar_id', 'bar_name']));
         return accumulator;
-    }, []));
+    }, [])});
 }
 
 module.exports = {
